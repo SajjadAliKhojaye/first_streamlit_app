01860c99b582dc6ebd707b4cc4471c593829ee28
Sajjad Ali <59386007+SajjadAliKhojaye@users.noreply.github.com>
2022-08-15 05:44:06 -0700

    Update streamlit_app.py
    imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,9 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocardo Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+    
+ import pandas as pd
+
+df = pd.read_csv(' https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(df)
